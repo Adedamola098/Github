06f07c8fdce71f3aebc41c83514aa8f83f2297e1
Adedamola098 <ayomikunadegboyega@gmail.com>
2025-03-26 11:31:04 +0100

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState()
 
   return (
     <>
@@ -16,12 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p> 
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
